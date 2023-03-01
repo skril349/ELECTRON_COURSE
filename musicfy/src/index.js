cdea7dc7a9870b76cb5a41fd7dcfad2f547e3738
skril349 <tvives.cabaleiro@gmail.com>
2023-03-01 12:34:13 +0100

    adding scss and semantic ui react

--- musicfy/src/index.js
+++ musicfy/src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import "semantic-ui-css/semantic.min.css"
+import "./scss/global.scss";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
