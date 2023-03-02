4274c6ed120e7137cf4af99723ebc0c5f0b75883
skril349 <tvives.cabaleiro@gmail.com>
2023-03-02 11:06:49 +0100

    adding register form styles

--- musicfy/src/Components/RegisterForm/RegisterForm.js
+++ musicfy/src/Components/RegisterForm/RegisterForm.js
@@ -1,13 +1,26 @@
 import React from 'react'
-import { Button } from 'semantic-ui-react'
+import {  Form, Icon } from 'semantic-ui-react'
+import "./RegisterForm.scss"
+
 export function RegisterForm(props) {
   const {openLogin, goBack} = props;
 
   return (
-    <div style={{backgroundColor:'#f00'}}>
-      <h1>Register Frm</h1>
-      <Button primary onClick={openLogin}>Login</Button>
-      <Button secondary onClick={goBack}>Atras</Button>
+    <div className='register-form'>
+      <h1>Empieza a escuchar con una cuenta de Musicfy gratis</h1>
+      <Form>
+        <Form.Input placeholder="Correo electrónico" icon="mail outline" type="text" error={true} />
+        <Form.Input placeholder="Contraseña" icon={<Icon name='eye' link onClick={()=>console.log("show password")} />} type="password" />
+        <Form.Input placeholder="Como deberiamos llamarte?" icon="user circle outline" type="text" />
+        <Form.Button  primary fluid type="submit">Continuar</Form.Button>
+
+
+      </Form>
+      <div className='register-form__options'>
+        <p onClick={goBack}>Volver</p>
+        <p>Ya tienes Musicfy? <span onClick={openLogin}>Iniciar sesión</span></p>
+
+      </div>
     </div>
   )
 }
