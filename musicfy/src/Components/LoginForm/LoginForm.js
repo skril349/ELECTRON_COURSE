371be623cd88af6a7aa2e7179d8b68b9ec0cd797
skril349 <tvives.cabaleiro@gmail.com>
2023-03-02 12:22:58 +0100

    adding login

--- musicfy/src/Components/LoginForm/LoginForm.js
+++ musicfy/src/Components/LoginForm/LoginForm.js
@@ -1,13 +1,45 @@
-import React from 'react'
-import { Button } from 'semantic-ui-react'
+import React, {useState} from 'react'
+import { Form, Icon } from 'semantic-ui-react'
+import "./LoginForm.scss"
+import { useFormik } from 'formik';
+import { initialValues, validationSchema } from './LoginForm.data';
+import { Auth } from '../../api';
+
+const auth = new Auth();
+
 export function LoginForm(props) {
   const {openRegister, goBack} = props;
+  const [showPassword, setShowPassword] = useState(false)
+  const onShowHiddenPassword = ()=>setShowPassword((prevState)=>!prevState)
+
+  const formik = useFormik({
+    initialValues:initialValues(),
+    validationSchema:validationSchema(),
+    validateOnChange:false,
+    onSubmit:async (formValue)=>{
+      try {
+        await auth.login( formValue.email, formValue.password)
+      } catch (error) {
+        
+      }
+    }
+  });
 
   return (
-    <div style={{backgroundColor:'#f0f'}}>
-      <h1>Login Form</h1>
-      <Button primary onClick={openRegister}>Registro</Button>
-      <Button secondary onClick={goBack}>Atras</Button>
+    <div className='login-form'>
+      <h1>Musica para todos</h1>
+      <Form onSubmit={formik.handleSubmit}>
+        <Form.Input name="email" type='text' placeholder="Correo electrónico" icon="mail outline" onChange={formik.handleChange} value={formik.values.email} error={formik.errors.email}/>
+        <Form.Input name="password" placeholder="Contraseña" icon={<Icon name={showPassword?"eye slash":"eye"} link onClick={onShowHiddenPassword} />} type={showPassword?"text":"password"}  onChange={formik.handleChange} value={formik.values.password} error={formik.errors.password} />
+        <Form.Button  primary fluid type="submit" loading={formik.isSubmitting}>Iniciar sesión</Form.Button>
+
+      </Form>
+
+      <div className='login-form__options'>
+        <p onClick={goBack}>Volver</p>
+        <p>No tienes cuenta? <span onClick={openRegister}>Registarse</span></p>
+
+      </div>
     </div>
   )
 }
--- musicfy/src/api/auth.js
+++ musicfy/src/api/auth.js
@@ -1,4 +1,4 @@
-import {getAuth, createUserWithEmailAndPassword, signOut} from "firebase/auth"
+import {getAuth, createUserWithEmailAndPassword, signOut, signInWithEmailAndPassword} from "firebase/auth"
 
 export class Auth{
     async register(email, password){
@@ -14,6 +14,15 @@ export class Auth{
         try {
             const auth = getAuth();
             await signOut(auth);
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async login(email, password){
+        try {
+            const auth = getAuth();
+            await signInWithEmailAndPassword(auth, email, password)
         } catch (error) {
             throw error;
         }
