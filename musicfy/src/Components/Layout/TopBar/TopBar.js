40f04b4c371a0761bee4886f0b1dd598dce2baf4
skril349 <tvives.cabaleiro@gmail.com>
2023-03-02 18:39:08 +0100

    finishing top bar

--- musicfy/src/Components/Layout/TopBar/TopBar.js
+++ musicfy/src/Components/Layout/TopBar/TopBar.js
@@ -1,21 +1,34 @@
 import React from "react";
 import { Icon, Image } from "semantic-ui-react";
 import { Link, useNavigate } from "react-router-dom";
+import { Auth, User } from "../../../api";
 import "./TopBar.scss";
+import { defaultUser } from "../../../assets";
+
+const auth = new Auth();
+const user = new User();
 
 export function TopBar() {
   const navigation = useNavigate();
+
+  const userData = user.getMe();
+
+  const displayName = userData.displayName || "Mi Cuenta";
+  const avatar = userData.photoURL || defaultUser;
+
   const goBack = () => {
     navigation(-1);
   };
+
   return (
     <div className="top-bar">
       <Icon name="angle left" className="top-bar__back" link onClick={goBack} />
       <div className="top-bar__right">
         <Link to="/profile">
-          <span>username</span>
+          <Image src={avatar} avatar />
+          <span>{displayName}</span>
         </Link>
-        <Icon name="power" onClick={() => console.log("logout")} link />
+        <Icon name="power" onClick={auth.logout} link />
       </div>
     </div>
   );
