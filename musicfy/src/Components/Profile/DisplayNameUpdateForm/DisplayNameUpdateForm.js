11c891257d13a7efcf28693d4d3a89264a8fff47
skril349 <tvives.cabaleiro@gmail.com>
2023-03-03 15:46:24 +0100

    creating display name form

--- musicfy/src/Components/Profile/DisplayNameUpdateForm/DisplayNameUpdateForm.js
+++ musicfy/src/Components/Profile/DisplayNameUpdateForm/DisplayNameUpdateForm.js
@@ -1,12 +1,36 @@
 import React from "react";
 import { Form } from "semantic-ui-react";
+import { useFormik } from "formik";
+import { initialValues, validationSchema } from "./DisplayNameUpdateForm.data";
+import { User } from "../../../api";
+import { async } from "@firebase/util";
+
+const userController = new User();
 
 export function DisplayNameUpdateForm(props) {
   const { onCloseModal } = props;
+
+  const { displayName } = userController.getMe();
+
+  const formik = useFormik({
+    initialValues: initialValues(displayName),
+    validationSchema: validationSchema(),
+    validationOnChange: false,
+    onSubmit: async (formValue) => {
+      console.log(formValue);
+    },
+  });
+
   return (
-    <Form>
-      <Form.Input name="displayName" placeholder="Nombre y apellodos" />
-      <Form.Button type="submit" primary fluid>
+    <Form onSubmit={formik.handleSubmit}>
+      <Form.Input
+        name="displayName"
+        placeholder="Nombre y apellodos"
+        value={formik.values.displayName}
+        onChange={formik.handleChange}
+        error={formik.errors.displayName}
+      />
+      <Form.Button loading={formik.isSubmitting} type="submit" primary fluid>
         Actualizar nombre
       </Form.Button>
     </Form>
