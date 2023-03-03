6ad3f30d41efee162022c7986169e27f26eba8af
skril349 <tvives.cabaleiro@gmail.com>
2023-03-03 13:01:02 +0100

    adding dropZone to avatar

--- musicfy/src/Components/Profile/AvatarUpdate/AvatarUpdate.js
+++ musicfy/src/Components/Profile/AvatarUpdate/AvatarUpdate.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useCallback } from "react";
 import "./AvatarUpdate.scss";
 import { Image } from "semantic-ui-react";
 import { defaultUser } from "../../../assets";
+import { useDropzone } from "react-dropzone";
 
 export function AvatarUpdate() {
+  const onDrop = useCallback(async (acceptedFile) => {
+    console.log(acceptedFile);
+  });
+
+  const { getRootProps, getInputProps } = useDropzone({ onDrop });
   return (
-    <div className="avatar-update">
+    <div className="avatar-update" {...getRootProps()}>
+      <input {...getInputProps()} />
       <Image src={defaultUser} />
     </div>
   );
