fa84b419f4a48052a3582f5edb8a365bc6730a1d
skril349 <tvives.cabaleiro@gmail.com>
2023-03-07 12:23:13 +0100

    adding classNames function

--- musicfy/src/Components/Artist/NewArtistForm.js
+++ musicfy/src/Components/Artist/NewArtistForm.js
@@ -3,10 +3,13 @@ import "./NewArtistForm.scss";
 import { Form, Image } from "semantic-ui-react";
 import { useDropzone } from "react-dropzone";
 import { noImage } from "../../assets";
+import { useFormik } from "formik";
+import classNames from "classnames";
+import { initialValues, validationSchema } from "./NewArtistForm.data";
 
 export function NewArtistForm(props) {
   const { onClose } = props;
-  const [image, setImage] = useState(noImage);
+  const [image, setImage] = useState(null);
 
   const onDrop = useCallback(async (acceptedFile) => {
     console.log(acceptedFile);
@@ -14,14 +17,34 @@ export function NewArtistForm(props) {
 
   const { getRootProps, getInputProps } = useDropzone({ onDrop });
 
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: validationSchema(),
+    validateOnChange: false,
+    onSubmit: async (formValue) => {
+      console.log(formValue);
+    },
+  });
+
   return (
-    <Form className="new-artist-form">
-      <div {...getRootProps()} className="new-artist-form__banner">
+    <Form className="new-artist-form" onSubmit={formik.handleSubmit}>
+      <div
+        {...getRootProps()}
+        className={classNames("new-artist-form__banner", {
+          error: formik.errors.file,
+        })}
+      >
         <input {...getInputProps()}></input>
-        <Image src={image} />
+        <Image src={image || noImage} className={classNames({ full: image })} />
       </div>
-      <Form.Input name="name" placeholder="Nombre del artista" />
-      <Form.Button type="submit" primary fluid>
+      <Form.Input
+        name="name"
+        placeholder="Nombre del artista"
+        value={formik.values.name}
+        onChange={formik.handleChange}
+        error={formik.errors.name}
+      />
+      <Form.Button type="submit" primary fluid loading={formik.isSubmitting}>
         Crear artista
       </Form.Button>
     </Form>
