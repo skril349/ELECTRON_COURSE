a5d18def5929e894c759f7c4c18cd7552ef7fbb2
skril349 <tvives.cabaleiro@gmail.com>
2023-03-07 16:36:02 +0100

    adding formik to album form

--- musicfy/src/Components/Albums/AddAlbumForm/AddAlbumForm.js
+++ musicfy/src/Components/Albums/AddAlbumForm/AddAlbumForm.js
@@ -2,11 +2,23 @@ import React from "react";
 import { Form, Image } from "semantic-ui-react";
 import "./AddAlbumForm.scss";
 import classNames from "classnames";
+import { useFormik } from "formik";
+import { initialValues, validationSchema } from "./AddAlbumForm.data";
+import { async } from "@firebase/util";
 
 export function AddAlbumForm(props) {
   const { onClose } = props;
+
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: validationSchema(),
+    validateOnChange: false,
+    onSubmit: async (formValue) => {
+      console.log(formValue);
+    },
+  });
   return (
-    <Form className="add-album-form">
+    <Form className="add-album-form" onSubmit={formik.handleSubmit}>
       <div className="add-album-form__content">
         <div
           className={classNames("add-album-form__content-image", {
@@ -16,7 +28,13 @@ export function AddAlbumForm(props) {
           <Image src={null} className={classNames({ full: null })} />
         </div>
         <div className="add-album-form__content-inputs">
-          <Form.Input name="name" placeholder="nombre del album" />
+          <Form.Input
+            name="name"
+            placeholder="nombre del album"
+            value={formik.values.name}
+            onChange={formik.handleChange}
+            error={formik.errors.name}
+          />
           <Form.Dropdown
             placeholder="el álbum pertenece..."
             fluid
@@ -26,7 +44,7 @@ export function AddAlbumForm(props) {
           />
         </div>
       </div>
-      <Form.Button type="submit" primary fluid>
+      <Form.Button type="submit" primary fluid loading={formik.isSubmitting}>
         Crear album
       </Form.Button>
     </Form>
