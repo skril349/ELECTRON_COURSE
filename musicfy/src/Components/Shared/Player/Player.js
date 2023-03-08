0a2137157132e244379854443fc093db36f0f76b
skril349 <tvives.cabaleiro@gmail.com>
2023-03-08 12:14:22 +0100

    adding react-player

--- musicfy/src/Components/Shared/Player/Player.js
+++ musicfy/src/Components/Shared/Player/Player.js
@@ -1,13 +1,23 @@
 import React from "react";
 import "./Player.scss";
 import { Progress, Icon } from "semantic-ui-react";
+import ReactPlayer from "react-player";
+import { usePlayer } from "../../../hooks";
 
 export function Player() {
-  const playing = false;
+  const { playing, song, pause, resume, volume } = usePlayer();
   return (
     <div className="player">
       <Icon name={playing ? "pause circle outline" : "play circle outline"} />
       <Progress progress="value" value={90} total={100} size="tiny" />
+
+      <ReactPlayer
+        url={song?.file}
+        playing={playing}
+        height={0}
+        width={0}
+        volume={volume}
+      />
     </div>
   );
 }
